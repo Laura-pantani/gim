61fe12a8ffc963ad361681739a98830e11b4b9c5
Laura-pantani <lalla.pantani@gmail.com>
2025-06-10 10:33:41 +0200

    Create sketch.js

---
+++ esercizio_3A/esempi/orologio_finale/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+	background(255, 0, 0)
+
+
+}
+
